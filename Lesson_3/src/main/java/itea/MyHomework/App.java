ee20490506a8dedf267bf0863bcc421af131191e
zf5bank <zf5bank@gmail.com>
2021-02-16 09:32:47 +0200

    Добавил модуль Lesson_3

---
+++ Lesson_3/src/main/java/itea/MyHomework/App.java
@@ -0,0 +1,13 @@
+package itea.MyHomework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
